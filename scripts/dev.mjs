360e8dda4b1e2fd2de7c1e8f90af91803c646ddf
Gilles4913 <gilles.guerrin@a2display.fr>
2025-09-10 15:26:04 +0200

    Ajout d'un serveur sur le port 3000

--- scripts/dev.mjs
+++ scripts/dev.mjs
@@ -1,3 +1,3 @@
 import { execSync } from 'node:child_process';
-const cmd = `npx concurrently -n operator,display -c auto "pnpm --filter @app/operator dev" "pnpm --filter @app/display dev"`;
+const cmd = `npx concurrently -n home,operator,display -c auto "pnpm --filter @app/home dev" "pnpm --filter @app/operator dev" "pnpm --filter @app/display dev"`;
 execSync(cmd, { stdio: 'inherit' });
